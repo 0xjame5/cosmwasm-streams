a4cb21a97ec41a785c7577c43f8a55aec49fb38a
James <11054922+entrancedjames@users.noreply.github.com>
2022-06-05 12:45:44 -0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -9,46 +9,55 @@ use substreams::{log, proto};
 // use cosmrs::proto::cosmos::distribution::v1beta1::MsgWithdrawDelegatorReward as ProtoMsgWithdraw;
 // use cosmrs::proto::types::Block as RawBlock;
 
-use cosmrs::proto::tendermint::types::Block as RawBlock;
+// use cosmrs::proto::tendermint::types::Block as RawBlock;
 
 
 #[substreams::handlers::map]
-fn map_hello_world(block: RawBlock) -> Result<pb::cosmos::Header, substreams::errors::Error>  {
-
-
-    // substreams::register_panic_hook();
-
-    // let block: pb::cosmos::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
-
-    // let block: RawBlock = proto::decode_ptr(block_ptr, block_len).unwrap();
-
-    // cosmrs::proto::cosmos::
+fn map_hello_world(block: pb::cosmos::Block) -> Result<pb::cosmos::Header, substreams::errors::Error>  {
     // We need to update and ask their team to use the definitions that are gneerally defined by the proto buffers.
     // This is the thrift definition of finding and grabbing
     
-    // let block = cosmrs::proto::dec
-
-    // let block = cosmrs::tendermint::Block::decode_;
-        // let block: cosmrs::tendermint::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
-
-    // let block: cosmrs::tendermint::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
-
-    // None of these blocks have transactions.
-    log::println(format!("Found N number of transactions in this block! {:?}", block.data));
+    // log::println(format!("Found N number of transactions in this block! {:?}", block.transactions));
 
     
+    
+
     // So we dont got shit here. Where can we data to start showing stuff.
     // What can we do now, idk. If we can get some more data...
-    // match Tx::from_bytes(tx_response.tx.as_bytes()) {
+    // We are one of the first using substreams with Graph TECH!!
+    
+
+    for tx_result in block.transactions {
+        if let Some(x) = tx_result.tx {
+            if let Some(tx_body) = x.body {
+                for msg in tx_body.messages {
+                    log::println(format!("Tx Dataobject {:?}", msg.type_url));
+                    // if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
+                        // log::println(format!("Tx Dataobject {:?}", msg));
+                    // }
+                }
+            }
+        }
+    }
+
     // for tx_result in block.transactions {
+    //         if let Some(x) = tx_result.tx {
+    //             if let Some(tx_body) = x.body {
+    //                 for msg in tx_body.messages {
+    //                     if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
+    //                         log::println(format!("Tx Dataobject {:?}", msg));
+    //                     }
+    //                 }
+    //             }
+    //         }
+    // }
+
     //     // Tx::from_bytes(tx_result.tx.as_bytes());
 
     //     if let Some(x) = tx_result.tx {
     //         if let Some(tx_body) = x.body {
     //             for msg in tx_body.messages {
     //                 log::println(format!("Tx Dataobject {:?}", msg.type_url));
-                    
-                    
     //                 if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
     //                     log::println(format!("Tx Dataobject {:?}", msg));
                         
@@ -96,18 +105,10 @@ fn map_hello_world(block: RawBlock) -> Result<pb::cosmos::Header, substreams::er
     //     // }
     // }
 
-    // let header: core::option::Option<pb::cosmos::Header> = block.header;
-    // match header {
-    //     Some(h) => {
-    //         // log::println(format!(
-    //         //     "Header at height {} with hash {} and chain_id {}",
-    //         //     h.height,
-    //         //     utils::address_pretty(h.hash.as_slice()), h.chain_id
-    //         // ));
-    //         substreams::output(h);
-    //     }
-    //     None => println!("No Header in block."),
-    // }
+    let header: core::option::Option<pb::cosmos::Header> = block.header;
 
-    todo!()
+
+    Ok(header.unwrap())
+
+    
 }
