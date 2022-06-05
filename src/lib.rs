2de3d6e273ff467eda11d2dafb1ca17e576a881c
James <11054922+entrancedjames@users.noreply.github.com>
2022-06-05 13:08:58 -0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -20,43 +20,26 @@ fn map_hello_world(block: pb::cosmos::Block) -> Result<pb::cosmos::Header, subst
     // log::println(format!("Found N number of transactions in this block! {:?}", block.transactions));
 
     
-    
-
-    // So we dont got shit here. Where can we data to start showing stuff.
-    // What can we do now, idk. If we can get some more data...
-    // We are one of the first using substreams with Graph TECH!!
-    
-
     for tx_result in block.transactions {
         if let Some(x) = tx_result.tx {
             if let Some(tx_body) = x.body {
                 for msg in tx_body.messages {
-                    log::println(format!("Tx Dataobject {:?}", msg.type_url));
-                    // if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
-                        // log::println(format!("Tx Dataobject {:?}", msg));
-                    // }
+                    if ENABLE_DEMO_1 {
+                        log::println(format!("Tx Dataobject {:?}", msg.type_url));
+                    }
+
+                    if ENABLE_DEMO_2 {
+                        if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
+                            log::println(format!("Tx Dataobject {:?}", msg));
+                        }
+                    }                    
                 }
             }
         }
     }
 
-    // for tx_result in block.transactions {
-    //         if let Some(x) = tx_result.tx {
-    //             if let Some(tx_body) = x.body {
-    //                 for msg in tx_body.messages {
-    //                     if msg.type_url == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward" {
-    //                         log::println(format!("Tx Dataobject {:?}", msg));
-    //                     }
-    //                 }
-    //             }
-    //         }
-    // }
-
-
     let header: core::option::Option<pb::cosmos::Header> = block.header;
 
-
     Ok(header.unwrap())
-
     
 }
